44efd9a69fb6011494cf92076aa9ab51b4eac2f0
Krituraaj Anand <kjais2000@gmail.com>
2022-03-26 19:52:31 +0530

    Added Filter functionality in ProductProvider and created Filter component

--- src/contexts/products/reducer.js
+++ src/contexts/products/reducer.js
@@ -1,28 +1,35 @@
-/**
- * 
- * @param {object} state : The Products state
- * @param {object} action : Containes the type of action and payload
- * @returns {object} Modified state
- */
-export const productsReducer = (state,{type,payload}) => {
-    const {productsList} = state
-    switch (type) {
-        case "addtoWishlist":
-            return {...state,productsList:productsList.map(product=> product.id === payload ? ({...product,addedToWishList: true}) : product )};
-        case "removeFromWishlist":
-            return {...state,productsList:productsList.map(product=> product.id === payload ? ({...product,addedToWishList: false}) : product )};
-        case "decreaseAvailability":
-            return {...state, productsList:productsList.map(product=> product.id === payload.id ? {...product,available: product.available - payload.decreaseBy } : product)}
-        case "increaseAvailability":
-            return {...state, productsList:productsList.map(product=> product.id === payload.id ? {...product,available: product.available + payload.increaseBy } : product)}        
-        case "addToCart":
-            return {...state,productsList:productsList.map(product=> product.id === payload.id ? ({...product,addedToCart: true, available: product.available - payload.quantity }) : product )};
-        case "removeFromCart":
-            return {...state,productsList:productsList.map(product=> product.id === payload.id ? ({...product,addedToCart: false, available: product.available + payload.quantity }) : product )};
-        case "updateProductsList":
-            return {...state,productsList:[...payload]}
-        default: 
-            return state;
-        
-    }
-}
+import { findMaxRoundedPrice } from "./utilities";
+export const productsReducer = (state, { type, payload }) => {
+  const defaultPriceLimit = findMaxRoundedPrice(state.productsList).maxRoundedPrice;
+  switch (type) {
+    case "updateProductsList":
+      return { ...state, productsList: [...payload], priceLimit: findMaxRoundedPrice(payload).maxRoundedPrice };
+    case "setFiltersAndSorts":
+      return { ...state, runFilterFunction:true , [payload.name]: payload.value };
+    case "setCategory":
+      if(state.category === payload) return state; // Do not perform any state updation if the new category is the same as the current one
+      return { // Reset all the filters if a new category is selected ( a new category page ) 
+        ...state, 
+        category:payload,
+        runFilterFunction:false,
+        sortBy: "",
+        includeOutOfStock: false,
+        brands:[],
+        ratings:1,
+        priceLimit: defaultPriceLimit
+      }
+    case "resetFilter":
+      return {
+        ...state,
+        category:"all",
+        runFilterFunction:false,
+        sortBy: "",
+        includeOutOfStock: false,
+        brands:[],
+        ratings:1,
+        priceLimit: defaultPriceLimit
+      };
+    default:
+      return state;
+  }
+};
---
+++ src/contexts/products/utilities.js
@@ -0,0 +1,85 @@
+/**
+ *
+ * @param {array} productList : An Array of product objects, each object having a price property
+ * @returns {number} Maximum rounded price 
+ */
+ export const findMaxRoundedPrice = (productList) => {
+    let divisor = 10;
+  
+    const maxPrice = Math.round(
+      productList.reduce(
+        (acc, curr) => (Number(curr.price) > acc ? Number(curr.price) : acc),
+        0
+      )
+    );
+  
+    while (Math.floor(maxPrice / divisor) !== 0) {
+      divisor *= 10;
+    }
+    const stepsValue = divisor / 10;
+    const stepsCount = Math.floor(maxPrice / stepsValue);
+    const maxRoundedPrice = stepsValue * stepsCount + stepsValue;
+    return { maxRoundedPrice, stepsCount, stepsValue };
+  };
+  
+  const sortByPrice = (list, { sortBy }) => {
+    let sortedList = [...list];
+    sortedList.sort((a, b) => Number(a.price) - Number(b.price));
+    switch (sortBy) {
+      case "priceAsc":
+        return sortedList;
+      case "priceDesc":
+        return sortedList.reverse();
+      default:
+        return [...list];
+    }
+  };
+  
+  const filterOutOfStock = (list, { includeOutOfStock }) =>
+    includeOutOfStock ? list : [...list].filter((item) => item.inStock);
+  
+  const filterByPrice = (list, { priceLimit }) =>
+    [...list].filter((item) => Number(item.price) <= Number(priceLimit));
+  
+  const filterByRatigs = (list, { ratings }) =>
+    [...list].filter((item) => item.ratings > ratings);
+  
+  const filterByBrands = (list, { brands }) =>
+    brands[0] ? [...list].filter((item) => brands.includes(item.brand)) : list;
+  
+  const filterByCategory = (list, { category }) =>
+    category ? [...list].filter((item) => item.categoryName === category) : list;
+  
+  const functionalChaining =
+    (filterParams, ...functions) =>
+    (productsList) => {
+      return functions.reduce(
+        (accum, curr) => curr(accum, filterParams),
+        productsList
+      );
+    };
+  
+  export const applyFilterAndSorts = (state) => {
+    const {
+      category,
+      productsList,
+      sortBy,
+      includeOutOfStock,
+      priceLimit,
+      ratings,
+      brands,
+    } = state;
+    const composedFunctions = functionalChaining(
+      { category, sortBy, includeOutOfStock, priceLimit, ratings, brands },
+      sortByPrice,
+      filterOutOfStock,
+      filterByPrice,
+      filterByRatigs,
+      filterByBrands,
+      filterByCategory
+    );
+  
+    let updatedList = composedFunctions(productsList);
+    return updatedList;
+  };
+  
